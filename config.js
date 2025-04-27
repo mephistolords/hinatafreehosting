c53f6fc018d73c2a09c717a339934efadc5e1faf
mephistolords <kaisershakib29@gmail.com>
2025-04-27 12:42:57 +0600

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
         clientId: "a568b55af1d940aca52ea8fe02f0d93b",
         clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
     },
-    botToken: "",
+    botToken: "MTM2NTYzOTU4Mzg0ODcyNjU0OQ.Ga9v0O.6VTZ6iYtL7l6AhbJvRGpQICPkxBmgWmPWswcN0",
     embedColor: "#0061ff"
 };
